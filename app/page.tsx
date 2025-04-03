8aa2fdfb3f228c29a01e51df2c09dd67a23feeb7
Karasaev Magomed <rostovmen95@gmail.com>
2025-04-03 21:19:09 +0300

    add sortPopup and topBar components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Categories, Container, Title} from "@/components/shared";
+import {Categories, Container, SortPopup, Title, TopBar} from "@/components/shared";
 
 
 export default function Home() {
@@ -6,8 +6,10 @@ export default function Home() {
         <>
             <Container className={"mt-10"}>
                 <Title text={"Все пиццы"} size={"lg"} className={"font-extrabold"}/>
-                <Categories/>
             </Container>
+            <TopBar/>
+
+            <div style={{height: '3000px'}}></div>
         </>
     );
 }
