18c87a657a70ad291f6d5be05f62161658f91d0c
Karasaev Magomed <rostovmen95@gmail.com>
2025-04-05 08:42:41 +0300

    add productCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import {Categories, Container, Filters, SortPopup, Title, TopBar} from "../components/shared";
+import {Container, Filters, Title, TopBar} from "../components/shared";
+import {ProductCard} from "@/components/shared/product-card";
 
 
 export default function Home() {
@@ -17,7 +18,7 @@ export default function Home() {
 
                     <div className={"flex-1"}>
                         <div  className={"flex flex-col gap-16"}>
-                            список товаров
+                            <ProductCard id={0} name={"Баварская"} price={500} imageUrl={"https://media.dodostatic.net/image/r:584x584/019591a2e222794a81731c99f3cc34ec.avif"}/>
                         </div>
                     </div>
                 </div>
